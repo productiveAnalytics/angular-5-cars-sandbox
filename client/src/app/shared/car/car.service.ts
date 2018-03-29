d447f6d56e059951b28498b6bf91aaf102c3c731
ProductiveAnalytics <LChawathe@ProductiveAnalytics.com>
2018-03-29 02:12:57 -0500

    Fully implemented POST, PUT and DELETE along w/ GET

--- client/src/app/shared/car/car.service.ts
+++ client/src/app/shared/car/car.service.ts
@@ -4,10 +4,34 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CarService {
+  public API = '//localhost:8080';
+  public CARS_API = this.API + '/cars';
 
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<any> {
-	  return this.http.get('//localhost:8080/cool-cars');
+	  return this.http.get(this.API + '/cool-cars');
+  }
+  
+  get(id: string) {
+	  console.log('GET request');
+	  return this.http.get(this.CARS_API + '/' + id);
+  }
+  
+  save(car: any): Observable<any> {
+	  let result: Observable<Object>;
+	  if (car['href']) {
+		  console.log('PUT request');
+		  result = this.http.put(car.href, car);
+	  } else {
+		  console.log('POST request');
+		  result = this.http.post(this.CARS_API, car);
+	  }
+	  return result;
+  }
+  
+  remove(href: string) {
+	  console.log('DELETE request');
+	  return this.http.delete(href);
   }
 }
